f1b7dda74530fc3ac2455edc4aaf238b0bec0e7b
Cyril Kuhns <cyril.kuhns@uipath.com>
2020-06-21 21:19:55 -0400

    First commit

---
+++ HelloInput/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloInput
+{
+    public class HelloWord : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<double> FirstNumber { get; set; }
+
+        [Category("Input")]
+        public InArgument<double> SecondNumber { get; set; }
+
+        [Category("Output")]
+        public OutArgument<double> ResultNumber { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var firstNumber = FirstNumber.Get(context);
+            var secondNumber = SecondNumber.Get(context);
+            var squaredSum = System.Math.Pow(firstNumber + secondNumber, 2);
+            string message = "The squared sum is "+squaredSum;
+            string caption = "Result:";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            
+            MessageBox.Show(message, caption, buttons);
+            ResultNumber.Set(context, squaredSum);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+#HelloInput
+
+This project immplements the example project from the [UiPath Custom Activity guide](https://docs.uipath.com/activities/docs/creating-a-custom-activity). It additionally implements diplaying the result in a **MessageBox**.
+
+##Project management styles
+
+So far we have learned about these project management styles:
+
+* Waterfall
+* Agile
+  * Scrum
+  * Kanban
