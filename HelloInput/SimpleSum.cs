9e7318f60b9045ab2bef1edcdf0a8bc54cc1b814
Cyril Kuhns <cyril.kuhns@uipath.com>
2020-08-04 11:27:36 -0400

    Separate classes for activities; added bin,obj,.suo to gitignore; first pass at unit tests

---
+++ HelloInput/SimpleSum.cs
@@ -0,0 +1,28 @@
+﻿using System.Activities;
+using System.ComponentModel;
+
+namespace HelloInput
+{
+    public class SimpleSum : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<double> FirstNumber { get; set; }
+
+        [Category("Input")]
+        public InArgument<double> SecondNumber { get; set; }
+
+        [Category("Output")]
+        public OutArgument<double> ResultNumber { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var firstNumber = FirstNumber.Get(context);
+            var secondNumber = SecondNumber.Get(context);
+            var result = firstNumber + secondNumber;
+            ResultNumber.Set(context, result);
+        }
+
+
+    }
+}
